c5e1826bc096c9d0e2d61e64d7ecde56e2376f7d
magyar nightcore <mogyorosdavid@gmail.com>
2023-11-05 17:18:42 +0100

    ♻️Changed vector to int** array

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
-#include <vector>
 #include <queue>
 #include <limits>
 
 constexpr std::pair<int,int> offsets[4] {{0, 1}, {-1, 0}, {0, -1}, {1, 0} };
+
+template <typename T>
+class Matrix {
+public:
+    Matrix(int row, int col) {
+        data = new T*[row];
+        for (int i = 0; i < row; ++i) {
+            data[i] = new T[col];
+            for (int j = 0; j < col; ++j) {
+                data[i][j] = T{};
+            }
+        }
+        this->row = row;
+        this->col = col;
+    }
+    ~Matrix(){
+        for (int i = 0; i < row; ++i) {
+            delete[] data[i];
+        }
+        delete[] data;
+    }
+
+    T* operator[] (int i) const {
+        return data[i];
+    }
+
+    int row{};
+    int col{};
+private:
+    T **data;
+};
 
 int inputCharToInt(const char c) {
     switch (c) {
@@ -18,12 +48,11 @@ int inputCharToInt(const char c) {
     }
 }
 
-void getInputFromCin(std::vector<std::vector<int>>& input, std::pair<int,int>& start,const int m,const int n) {
-    for (int i = 0; i < m; ++i) {
+void getInputFromCin(Matrix<int> & input, std::pair<int,int>& start) {
+    for (int i = 0; i < input.row; ++i) {
         std::string tmp;
-        tmp.reserve(n);
         std::cin >> tmp;
-        for (int j = 0; j < n; ++j) {
+        for (int j = 0; j < input.col; ++j) {
             input[i][j] = inputCharToInt(tmp[j]);
             if (tmp[j] == 'T') start = std::make_pair(i,j);
         }
@@ -34,38 +63,44 @@ void getInputFromCin(std::vector<std::vector<int>>& input, std::pair<int,int>& s
     }
 }
 
-bool isCellValid(const std::vector<std::vector<int>>& input,const int x,const int y) {
-    return ( (x >= 0 && x < input.size()) && (y >= 0 && y < input[0].size()));
+bool isCellValid(const Matrix<int> & input,const int x,const int y) {
+    return ( (x >= 0 && x < input.row) && (y >= 0 && y < input.col));
 }
 
 
-void calculateDistanceMatrix(std::vector<std::vector<int>>& input, std::pair<int,int>& start) {
+void calculateDistanceMatrix(Matrix<int> & input,const std::pair<int,int>& start) {
     std::queue<std::pair<int,int>> queue;
     queue.push(start);
+    Matrix<bool> visited(input.row,input.col);
+    for (int i = 0; i < input.row; ++i) {
+        for (int j = 0; j < input.col; ++j) {
+            visited[i][j] = input[i][j] < 0;
+        }
+    }
 
     while (!queue.empty()) {
         std::pair<int,int> cell = queue.front();
+        int currDistance = std::max(input[cell.first][cell.second],0);
         queue.pop();
 
-        for (auto offset : offsets) {
+        for (auto const offset : offsets) {
             std::pair<int,int> adj {cell.first + offset.first, cell.second + offset.second};
 
-            if (isCellValid(input, adj.first, adj.second)) {
-                int currDistance = std::max(input[cell.first][cell.second],0);
-                if (input[adj.first][adj.second] == 0) {
-                    input[adj.first][adj.second] = currDistance + 1;
-                    queue.push(adj);
-                }
+            if (isCellValid(input, adj.first, adj.second) && !visited[adj.first][adj.second]) {
+                input[adj.first][adj.second] = currDistance + 1;
+                visited[adj.first][adj.second] = true;
+                queue.push(adj);
+
             }
         }
     }
 }
 
 
-int readGoalDistanceSum(const std::vector<std::vector<int>>& input) {
+int readGoalDistanceSum(const Matrix<int> & input) {
     int distancesum {0};
-    for (int i = 0; i < input.size(); ++i) {
-        for (int j = 0; j < input[i].size(); ++j) {
+    for (int i = 0; i < input.row; ++i) {
+        for (int j = 0; j < input.col; ++j) {
             if (input[i][j] < 0 && input[i][j] > -16) {
                 int mask = -input[i][j];
                 int closestDistance {std::numeric_limits<int>::max()};
@@ -85,9 +120,9 @@ int readGoalDistanceSum(const std::vector<std::vector<int>>& input) {
 int main(){
     int m, n;
     std::cin >> m >> n;
-    auto input = std::vector<std::vector<int>>(m, std::vector<int>(n));
+    Matrix<int> input(m,n);
     std::pair<int,int> startpos(-1,-1);
-    getInputFromCin(input, startpos, m, n);
+    getInputFromCin(input, startpos);
     calculateDistanceMatrix(input, startpos);
     std::cout << readGoalDistanceSum(input);
 }
